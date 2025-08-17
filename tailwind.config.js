2340c0fa6a62db05e27d4d4557d228b8b72d0dfd
IanDizon <dznn20@gmail.com>
2025-08-17 20:34:33 +0800

    Fix tailwind for chat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.js',
+  ],
+  safelist: [
+    'justify-start',
+    'justify-end',
+    'bg-yellow-300',
+    'bg-green-300',
+    'rounded-br-none',
+    'rounded-bl-none',
+    'bg-[#021908]',
+    'bg-[#F9FAF8]',
+    'bg-[#FFFFFF]',
+    'bg-[#BBCB2F]',
+    'text-[#021908]',
+    'text-[#02190888]',
+    'text-[#BBCB2F]',
+    'text-[#FEFA95]'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
